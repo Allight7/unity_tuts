a2518b74491f4b4b1481a3cc9252bb503af4daa8
Allight7 <allight.me@ya.ru>
2014-11-17 22:33:05 +0300

    roll a ball complete

--- roll a ball/Assets/Scripts/PlayerController.cs
+++ roll a ball/Assets/Scripts/PlayerController.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int  count;
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString();
+		if (count >= 5) {
+			winText.text = "You WIN!";
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -21,5 +33,13 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+			++count;
+			SetCountText ();
+		}
 	}
 }
